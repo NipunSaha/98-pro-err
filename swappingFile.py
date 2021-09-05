14fdcf83e382a768ab43edf2cf8a6be072b082a4
Nipun Saha <76034504+NipunSaha@users.noreply.github.com>
2021-09-05 20:22:07 +0530

    Update swappingFile.py

--- swappingFile.py
+++ swappingFile.py
@@ -6,7 +6,7 @@ def swapFileData():
     with open(file1, 'r') as a:
         data_a = a.read()
 
-	with open(file2, 'r') as b:
+    with open(file2, 'r') as b:
         data_b = b.read()
 
     with open(file1, 'w') as a:
